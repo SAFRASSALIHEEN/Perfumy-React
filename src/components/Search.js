9a614989681ed1e9ef240140afd5003e512b2d08
developer <developer@zecode.lk>
2024-10-04 19:11:33 +0530

    updated project

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+// search
+
+function Search() {
+    return (
+      <div class="searchbar">
+        <input type="text" placeholder="Search" />
+      </div>
+    )
+  }
+
+  export default Search
+  
